0b6dee9c72dc63c5cc74d4811f4d606b36c84ca0
Jobsidney <jobsidney67@gmail.com>
2022-10-22 16:13:09 +0300

    share area done

--- src/components/ShareContent.jsx
+++ src/components/ShareContent.jsx
@@ -4,18 +4,21 @@ import * as Fa from "react-icons/fa";
 
 function ShareContent({}) {
   return (
-    <div className='w-full h-[200px] bg-[#e3ebfa] rounded-xl'>
+    <div className='w-full h-fit bg-[#e3ebfa] rounded-xl pb-3'>
         <form >
             <textArea rows="3" placeholder='Share your content. . . . .' className='w-full h-full bg-transparent text-xl text-gray-500 p-5 outline-none' />
         </form>
         <div className='mx-5 flex flex-row justify-between'>
             <div className='flex flex-row space-x-3'>
-                <div className='border border-gray-500 rounded-full p-2'>
+                <div className='border h-fit border-gray-500 rounded-full p-2'>
                     <Fa.FaPlus className=' text-xl  text-gray-600 cursor-pointer'/>
                 </div>
-                <div className='border border-gray-500 rounded-full p-2'>
-                    <Fa.FaPlus className=' text-xl text-gray-600 cursor-pointer'/>
+                <div className='border h-fit border-gray-500 rounded-full p-2'>
+                    <Fa.FaRegImages className=' text-xl text-gray-600 cursor-pointer'/>
                 </div>
+            </div>
+            <div className=' border-gray-500  p-2'>
+            <Fa.FaTelegramPlane className=' text-4xl cursor-pointer text-[#3080ED]'/>
             </div>
         </div>
       
