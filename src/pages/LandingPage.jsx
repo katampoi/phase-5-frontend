70ad8dea561cb4b7ce373b4dd2a577ca8e3f5b86
Jobsidney <jobsidney67@gmail.com>
2022-10-22 14:07:58 +0300

    body sections

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,8 +3,19 @@ import NavBar from '../components/NavBar'
 
 function LandingPage() {
   return (
-    <div>
+    <div className=''>
       <NavBar/>
+      <section id="body" className='grid grid-cols-[440px,940px,440px] justify-between gap-3 mt-4'>
+        <section id="leftBar" className='w-[440px] h-[90vh] bg-red-900'>
+
+        </section>
+        <section id='middle' className='bg-blue-900 '>
+
+        </section>
+        <section id="leftBar" className=' h-[90vh] bg-red-900 '>
+
+        </section>
+      </section>
     </div>
   )
 }
