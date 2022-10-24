07df3e1bc5fb95ce295bd3837d7033c2adcae719
Jobsidney <jobsidney67@gmail.com>
2022-10-24 13:23:47 +0300

    User foowing

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function UserCard() {
   return (
-    <div className=' h-44 rounded-xl bg-[#e3ebfa] p-4'>
+    <div className=' h-44 rounded-xl bg-[#e3ebfa] p-4 flex flex-col justify-between'>
         <div className='flex flex-row space-x-3'>
             <img className='w-14 rounded-full h-14 object-cover' src="https://media.istockphoto.com/photos/portrait-concept-picture-id1016761216?k=20&m=1016761216&s=612x612&w=0&h=jEC8voGLjSyhdOO7EMQyrLtZ9m--TEUmd4X56sqyZk0=" alt="" />
         <div>
@@ -12,6 +12,17 @@ function UserCard() {
             </div>
         </div>
 
+        </div>
+
+        <div className=''>
+            <div>
+                <h2 className='font-semibold'>2.1k</h2>
+                <p>Following</p>
+            </div>
+            <div>
+                <h2 className='font-semibold'>2.1k</h2>
+                <p>Following</p>
+            </div>
         </div>
 
     </div>
