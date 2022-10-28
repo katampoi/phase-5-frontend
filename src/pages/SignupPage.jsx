d7457039f9a05aabc5efb123471395f656abac48
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-10-28 09:26:38 +0300

    creates the signup page

---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SignUp from "../components/Signup";
+
+function SignupPage(){
+    return(
+        <>
+        <div className='h-[80vh] bg-white-400 text-center login m-0 p-0 '>
+        <SignUp/>
+        </div>
+        </>
+    )
+}
+
+export default SignupPage
