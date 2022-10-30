cc669f213637fa0040eb7375fca425ea22ead543
Jobsidney <jobsidney67@gmail.com>
2022-10-30 17:53:37 +0300

    refactor

--- src/components/ShareContent.jsx
+++ src/components/ShareContent.jsx
@@ -4,8 +4,8 @@ import * as Fa from "react-icons/fa";
 import Axios from 'axios'
 function ShareContent({user}) {
     const [formDataa,setData] =useState()
-    const [mediaOn,setMediaOn] = useState(true)
-    const [mediaVidOn,setMediaVidOn] = useState(true)
+    const [mediaOn,setMediaOn] = useState(false)
+    const [mediaVidOn,setMediaVidOn] = useState(false)
     function handleChange(event) {
         const name=event.target.name;
         const value=event.target.value;
@@ -35,11 +35,22 @@ function ShareContent({user}) {
             console.log(formDataa);
         })   
     }
+    function handleSubmit(event){
+        event.preventDefault()
+        fetch("https://localhost3000/posts", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+          },
+            body: JSON.stringify(formDataa),
+          }).then(res=>res.json())
+          console.log(formDataa);
+          }
   return (
     <div className='w-full  h-44  bg-[#e3ebfa] rounded-xl pb-3'>
-        <form  className='p-2 px-5'>
-            <div className='flex space-x-2'>
-            <input type="text" placeholder='Title' onChange={handleChange} name='title' className='border px-2 border-gray-400 outline-none' />
+        <form onSubmit={handleSubmit}  className='p-2 px-5'>
+            <div className='flex space-x-5'>
+            <input type="text" placeholder='Title' onChange={handleChange} name='title' className='border w-[200px] px-2 bg-transparent border-b-gray-400 outline-none' />
             <div className=' text-gray-500'>
                 Category:
             <select name="category" onChange={handleChange}  className=' w-16 text-center outline-none bg-transparent border border-none'>
@@ -50,10 +61,10 @@ function ShareContent({user}) {
             </div></div>
             <textArea name='content' onChange={handleChange} rows="1" placeholder='Type your content. . . . .' className='w-full  bg-transparent text-md text-gray-500 p-3 outline-none' />
             <div className='flex flex-row'>
-                <input className={mediaOn ? "hidden" :'block'} type="file" onChange={(event)=>{
+                <input className={mediaOn ? "on" :'off'} type="file" onChange={(event)=>{
                     uploadFile(event.target.files)
                 }} />
-                <input className={mediaVidOn ? "hidden" :'block'} type="file" onChange={(event)=>{
+                <input className={mediaVidOn ? "on" :'off'} type="file" onChange={(event)=>{
                     uploadFile(event.target.files)
                 }} />
             </div>
@@ -75,4 +86,4 @@ function ShareContent({user}) {
   )
 }
 
-export default ShareContent
+export default ShareContent;
