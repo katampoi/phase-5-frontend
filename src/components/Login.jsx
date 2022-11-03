2147bf013d1d748fca76cec561267f81ba2318b0
David Ongaro <106276877+mweneh@users.noreply.github.com>
2022-11-03 21:56:49 +0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,50 +1,50 @@
 import React,{useState} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
-import LandingPage from '../pages/LandingPage';
+import { useNavigate } from 'react-router-dom'
 
 
-function Login({onLogin,loggedUser}) {
-  const [formData,setData] = useState({});
-  const navigate = useNavigate()
- 
+function Login({onLogin }) {
 
-    function handleChange(event) {
-    const name=event.target.name;
-    const value=event.target.value;
-    setData({
-    ...formData,
-    [name]: value,
-    })
+const [action,setAction] = useState(true);
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+const [setErrors] = useState([""]);
+const [isLoading, setIsLoading] = useState(false);
+const navigate=useNavigate();
+
+function handleAction(){
+  setAction(!action)
+}
+
+function handleSubmit(e){
+  e.preventDefault();
+  setIsLoading(true);
+  fetch("http://127.0.0.1:3000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  }).then((r) => {
+    setIsLoading(false);
+    if (r.ok) {
+      r.json().then((user) => onLogin(user))
+      navigate('/Landing')
+      alert('Login Successfull !...')
+    } else {
+      r.json().then((err) => setErrors(err.errors));
     }
-
-    function handleSubmit(event) {
-        event.preventDefault()
-      fetch("http://localhost:3000/login", {
-        method: "POST",
-        headers: {
-          "content-Type": "application/json",
-      },
-        body: JSON.stringify(formData),
-      }).then((res)=>res.json()).then(data=>{
-          loggedUser(data.id)
-          onLogin(data)
-          if (data.id) {
-            navigate('/Landing')
-            alert(data.id)
-          }
-
-        })
-    }
+  });
+}
+  
   return (
     <div className='flex flex-col  m-auto w-screen h-screen items-center justify-center'>
 
       <form onSubmit={handleSubmit} action="" className='flex flex-col space-y-10 md:w-[450px] '>
-        <input name='username' type="text" placeholder='Username' onChange={handleChange} className=' border border-black p-4 rounded-md bg-[#F5F7FB] text-black' />
-        <input name='password' type="password" placeholder='Password' onChange={handleChange} className='border border-black p-4 rounded-md bg-[#F5F7FB] text-blac' />
+        <input name='username' type="text" placeholder='Username' onChange={(e) => setUsername(e.target.value)} required className=' border border-black p-4 rounded-md bg-[#F5F7FB] text-black' />
+        <input name='password' type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} required className='border border-black p-4 rounded-md bg-[#F5F7FB] text-blac' />
 
         <div className='flex flex-row items-center justify-between'>
-          <button type='submit' className='bg-[#3080ED] px-12 py-2 rounded-[30px] text-white font-bold'><Link to="/Landing" element={<LandingPage userr={1}/>} >Login</Link></button>
-          {/* <button type='submit' classNameName='log' id='logIn'>{action?'Login' :'Delete'}</button> */}
+        <button type='submit' className='bg-[#3080ED] px-12 py-2 rounded-[30px] text-white font-bold'   >{action?'Login' :'Delete'}</button>
           <div className='text-[#3080ED] font-bold'>Forgot password?</div>
         </div>
         <p className='font-bold text-lg text-start'>Do not have an Account? <span className='text-sm text-[#3080ED]'>Create New</span></p>
@@ -54,6 +54,3 @@ function Login({onLogin,loggedUser}) {
   )
 }
 export default Login
-
-
-
