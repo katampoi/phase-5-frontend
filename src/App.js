f3ae7aaad75856fd51347a2e8870f4b865a2b04f
Jobsidney <jobsidney67@gmail.com>
2022-11-03 16:29:01 +0300

    dd

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ function App() {
     })
   },[])
 
-  if (user) {
+  if (user===null || user===undefined){
     console.log(user);
-    return <LandingPage user={user}/>
-  } else {
-    return <LoginPage onLogin={setUser} />;
+    return <LandingPage />
+  } else  {
+    return <LandingPage user={user}/>;
   }
 
 
@@ -34,7 +34,7 @@ function App() {
       <Router>
 
         <Routes>
-        <Route exact path="" element={<LandingPage/>}></Route>
+        <Route exact path="/Landing" element={<LandingPage/>}></Route>
         <Route exact path="/LoginPage" element={<LoginPage />}></Route>
         <Route exact path="/SignupPage" element={<SignupPage />}></Route>
         <Route exact path="/Category" element={<CategoryPage/>}></Route>
